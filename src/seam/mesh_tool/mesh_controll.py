a29a057cc63cb7d4598086505cc2284f8a6d4a94
MahiroGoto <55484233+MahiroGoto@users.noreply.github.com>
2020-07-12 17:37:28 +0200

    yes

--- src/seam/mesh_tool/mesh_controll.py
+++ src/seam/mesh_tool/mesh_controll.py
@@ -4,16 +4,20 @@ import numpy as np
 import open3d as o3d
 
 import stratum.utils.utils as utils
-import
+
+###
+import logging
+logger = logging.getLogger('logger')
+###
+
+
 
 def add_vertex_from_PtsList(mesh, points_list):
     mesh = Mesh()
 
-def create_mesh_from_layer_path_pts():
-    input_path = "G:/.shortcut-targets-by-id/19j2p-s21q1pYuoGXuk11Bu934R1tS8MU/MAS Thesis 2020/3_Prototype/000_Exploration/04_describe_printedShape/data/"
-    output_path = "G:/.shortcut-targets-by-id/19j2p-s21q1pYuoGXuk11Bu934R1tS8MU/MAS Thesis 2020/3_Prototype/000_Exploration/04_describe_printedShape/output/"
-    dataname = "sample.xyz"
-    point_cloud = np.loadtxt(input_path + dataname, skiprows=1)
+def create_mesh_from_layer_path_pts(output_path, point_cloud):
+
+    # point_cloud = np.loadtxt(input_path + dataname, skiprows=1)
 
     pt_cld = o3d.geometry.PointCloud()
     pt_cld.points = o3d.utility.Vector3dVector(point_cloud[:, :3])
@@ -30,6 +34,11 @@ def create_mesh_from_layer_path_pts():
     bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pt_cld, o3d.utility.DoubleVector([radius, radius * 2]))
 
     dec_mesh = bpa_mesh.simplify_quadric_decimation(10000)
+
+    o3d.io.write_triangle_mesh(output_path + "bpa_mesh.ply", dec_mesh)
+
+    logger.info("save mesh:" + "bpa_mesh.ply" )
+
 
 
 
